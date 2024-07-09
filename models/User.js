854a8c3c1beaebc03e5c98b251adadc0b0c3ad6f
Roko <rokoponjarac2@gmail.com>
2024-07-09 17:18:39 +0200

    hash user password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -34,6 +35,12 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true, versionKey: false }
 );
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.passsword = await bcrypt.hash(this.passsword, salt);
+  next();
+});
 
 const UserModel =
   mongoose.models.User || mongoose.model("User", UserSchema, "user");
