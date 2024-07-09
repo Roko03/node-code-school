a5a2bfd19805e5643740e7acaab8c6f98ef024b1
Roko <rokoponjarac2@gmail.com>
2024-07-09 11:25:01 +0200

    refactor server

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("Running...");
+const express = require("express");
+const app = express();
+
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Ej");
+});
+
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const server = async () => {
+  try {
+    app.listen(port, console.log("Server is running"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+server();
