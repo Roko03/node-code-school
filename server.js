2d950309b6d28eb73e22876beb407cc3e763cf5e
Roko <rokoponjarac2@gmail.com>
2024-07-09 15:30:58 +0200

    make 404 page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
 
 app.use(express.json());
 
@@ -9,6 +10,7 @@ app.get("/", (req, res) => {
   res.json("Ej");
 });
 
+app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
