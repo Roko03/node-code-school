5c81917129a4f0dcebeba38e687cf6fbc1fc62c3
Roko <rokoponjarac2@gmail.com>
2024-07-10 18:56:34 +0200

    make login route

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -11,6 +12,31 @@ const register = async (req, res) => {
     .json({ user: { username: user.username }, token });
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Molimo unesite email i šifru");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Korisnik sa unesenim emailom ne postoji");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Netočna šifra");
+  }
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ user: { username: user.username }, token });
+};
+
 module.exports = {
   register,
+  login,
 };
--- router/user.js
+++ router/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { register } = require("../controllers/user");
+const { register, login } = require("../controllers/user");
 
 router.route("/register").post(register);
+
+router.route("/login").post(login);
 
 module.exports = router;
