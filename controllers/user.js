8b66878e9cb30b2ba5e0b1a699b40816590ee033
Roko <rokoponjarac2@gmail.com>
2024-07-10 16:50:29 +0200

    make user register route

---
+++ controllers/user.js
@@ -0,0 +1,16 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+
+  const token = user.createJWT();
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { username: user.usermane }, token });
+};
+
+module.exports = {
+  register,
+};
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+require("express-async-errors");
+
 const express = require("express");
 const app = express();
 
@@ -7,11 +9,15 @@ const connectDB = require("./utils/connectDB");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const notFoundMiddleware = require("./middleware/not-found");
 
+const userRouter = require("./router/user");
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("Ej");
 });
+
+app.use("/api/v1/auth", userRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
