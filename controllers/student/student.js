9a485c686b8b5bb94adcadd3d7ef52fea9ba5512
Roko <rokoponjarac2@gmail.com>
2024-07-14 17:45:30 +0200

    refactor get student workshops

--- controllers/student/student.js
+++ controllers/student/student.js
@@ -7,20 +7,19 @@ const { NotFoundError } = require("../../errors");
 const getStudentWorkshop = async (req, res) => {
   const { _id: userId } = req.user;
 
-  const workshop = await LoginWorkshop.find({ user_id: userId })
-    .select("-user_id")
-    .lean()
-    .populate([
-      {
-        path: "workshop_id",
-        populate: {
-          path: "user_id",
-          select: "username email",
-        },
-        select: "-createdAt -updatedAt",
-      },
-    ]);
-  res.status(StatusCodes.OK).json({ workshop });
+  const allWorkshop = await Workshop.find({}).lean();
+
+  const studentWorkshop = await LoginWorkshop.find({ user_id: userId })
+    .distinct("workshop_id")
+    .lean();
+
+  const stringStudentWorkshop = studentWorkshop.map((item) => item.toString());
+
+  const workshops = allWorkshop.filter((workshop) =>
+    stringStudentWorkshop.includes(workshop._id.toString())
+  );
+
+  res.status(StatusCodes.OK).json({ workshops });
 };
 
 const getAllWorkshop = async (req, res) => {
